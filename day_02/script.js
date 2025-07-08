aa102f8683b6bc748b3c3f53117981b15dc55943
rutujaa121 <rutujaaugale@gmail.com>
2025-07-07 22:49:03 -0700

    Added Pages

--- day_02/script.js
+++ day_02/script.js
@@ -2,5 +2,16 @@
 // let litral_name = "value";
 // const constant_name = "value";
 
+
+
+// String 
 let userName = "Rutuja"
 console.log(typeof userName)
+
+let subject = "Javascript"
+console.log(typeof subject)
+
+
+// Boolean 
+let isLoggedIn = true
+console.log(typeof isLoggedIn)
