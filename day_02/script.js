037506c3da1a60ed2dbfa40e07254222eb270ff5
rutujaa121 <rutujaaugale@gmail.com>
2025-07-07 22:45:14 -0700

    Added Pages

--- day_02/script.js
+++ day_02/script.js
@@ -1,27 +1,4 @@
-
-// Addition 
-let a = 4;
-let b = 3;
-console.log(a + b);
-
-// Substraction 
-let c = 5;
-let d = 10;
-console.log(c - d);
-
-// Multiplication 
-let e = 6;
-let f = 5;
-console.log(e * f);
-
-// Division
-let g = 5;
-let h = 10;
-console.log(g / h);
-
-
-
-let abc = 2;
-let xyz = "2";
-console.log(abc === xyz);
+// var variable_name = "value";
+// let litral_name = "value";
+// const constant_name = "value";
 
