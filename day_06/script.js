2fdcb35e0c63080d277c46dbf904059792bf462e
rutujaa121 <rutujaaugale@gmail.com>
2025-07-09 19:54:38 -0700

    added

---
+++ day_06/script.js
@@ -0,0 +1,11 @@
+let month = "january"
+
+switch(month){
+    case "january":
+        console.log("31 days")
+        break;
+    case "february":
+        console.log("28 days")
+        break;
+    
+}
