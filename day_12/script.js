243f6c6bbc94377d057545235c69f762e847261a
rutujaa121 <rutujaaugale@gmail.com>
2025-07-17 19:32:05 -0700

    added

--- day_12/script.js
+++ day_12/script.js
@@ -4,3 +4,25 @@ let person = {
     city : "Newasa"
 };
 console.log(person);
+
+
+// ================        2         ================
+const mycity = {
+    name : "Shevgaon",
+    pincode : 414502,
+    famous_area : ["Shri Gajanan Maharaj","main-paith"]
+}
+console.log(mycity.name)
+console.log(mycity.pincode)
+console.log(mycity.famous_area[1])
+console.log(mycity.famous_area)
+console.log(mycity)
+
+
+// ================        3         ================
+
+
+
+// // ================        4         ================
+// let userName = "Rutuja"
+// console.log(userName.length)
