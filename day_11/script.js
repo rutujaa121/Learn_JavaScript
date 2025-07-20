480f554df77d511f467782244ac42aeb1646f9ac
rutujaa121 <rutujaaugale@gmail.com>
2025-07-20 08:56:10 -0700

    added

--- day_11/script.js
+++ day_11/script.js
@@ -1,7 +1,24 @@
 // ====================        1       ====================
 let arr = [1,2,3,4,5,6,7,8,9]
-console.log(arr)
+console.log(arr);
+//*************
+console.warn("This First Line Break..........!!!!!!");
 
 
-console.warn("This Line Break..........!!!!!!");
-// ====================        1       ====================
+// ====================        2       ====================
+let animal_name = ['dog','cat','horse']
+console.log(animal_name[1]);
+console.log(animal_name);
+//*************
+console.warn("This Second Line Break..........!!!!!!");
+
+
+// ====================        3       ====================
+let color = ['red','pink','green','purple']
+console.log(color[2]);
+console.log(color);
+//*************
+console.warn("This Third Line Break..........!!!!!!");
+
+
+// ====================        4       ====================
