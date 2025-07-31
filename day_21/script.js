68463e16f3278c565a9d62f255e09de650663bac
rutujaa121 <rutujaaugale@gmail.com>
2025-07-30 17:24:18 -0700

    added

--- day_21/script.js
+++ day_21/script.js
@@ -1,8 +1,18 @@
 // alert("hello");
 
-let date = new Date();
-console.log(date);
-console.log(date.getFullYear());
-console.log(date.getMonth());
-console.log(date.getMonth()+1);
-console.log(date.getDate())
+// let date = new Date();
+// console.log(date);
+// console.log(date.getFullYear());
+// console.log(date.getMonth());
+// console.log(date.getMonth()+1);
+// console.log(date.getDate());
+
+
+// console.warn("......====......");
+
+let date = new Date()
+console.log(date.getHours());
+console.log(date.getHours() % 12);
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+console.log(date.getMilliseconds());
