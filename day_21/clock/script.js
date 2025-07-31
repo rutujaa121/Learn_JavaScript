9a2db36e0c482421476e5db04792b94ebd1e0856
rutujaa121 <rutujaaugale@gmail.com>
2025-07-31 16:12:49 -0700

    added

--- day_21/clock/script.js
+++ day_21/clock/script.js
@@ -1,6 +1,8 @@
 let timeContainer = document.getElementById("timeContainer");
 const generateTime=()=>{
     let date = new Date();
-
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
     console.log(date)
 }
