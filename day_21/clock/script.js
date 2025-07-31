39e5dc090234d0209531f4def3d38d5b6865a91b
rutujaa121 <rutujaaugale@gmail.com>
2025-07-31 16:16:52 -0700

    added

--- day_21/clock/script.js
+++ day_21/clock/script.js
@@ -4,5 +4,10 @@ const generateTime=()=>{
     let hour = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
+    let milliSec = date.getMilliseconds();
+
+    timeContainer.innerHTML = `${hour%12||12}:: ${min} :: ${sec} <span id="mili-sec"> ${milliSec} </span>`
+
+    dateContainer.innerHTML
     console.log(date)
 }
