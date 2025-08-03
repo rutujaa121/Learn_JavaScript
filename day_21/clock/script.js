2ee299994f506fce788c55b6b4b8a61943703194
rutujaa121 <rutujaaugale@gmail.com>
2025-08-02 19:01:41 -0700

    added

--- day_21/clock/script.js
+++ day_21/clock/script.js
@@ -2,14 +2,17 @@ let timeContainer = document.getElementById("time-container");
 let dateContainer = document.getElementById("date-container");
 let months = ["jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
 
-const generateTime=()=>{
+const generateTime = () =>{
     let date = new Date();
     let hour = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
     let milliSec = date.getMilliseconds();
 
-    timeContainer.innerHTML = `${hour%12||12}:: ${min} :: ${sec} <span id="mili-sec"> ${milliSec} </span>`
+    timeContainer.innerHTML = `${hour %12 || 12}:: ${min} :: ${sec} <span id="mili-sec"> ${milliSec} </span>`
 
     dateContainer.innerHTML = `${date.getDate()} / ${months[date.getMonth()]} / ${date.getFullYear()}`
 }
+ setInterval(()=>{
+    ganarateTime()
+ },1)
